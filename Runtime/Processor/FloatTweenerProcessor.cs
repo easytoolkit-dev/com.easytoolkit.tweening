e02a9efd190292f3fd1f6ca292c0772883b5c58c
计都 <119787492+fuyouawa@users.noreply.github.com>
2026-01-28 10:40:48 +0800

    🎉初始化

---
+++ Runtime/Processor/FloatTweenerProcessor.cs
@@ -0,0 +1,23 @@
+using EasyToolkit.Core.Mathematics;
+using UnityEngine;
+
+namespace EasyToolkit.Tweening
+{
+    public class FloatLinearTweenerProcessor : AbstractTweenerProcessor<float, LinearTweenerEffect>
+    {
+        protected override float GetRelativeValue(float value, float relative)
+        {
+            return value + relative;
+        }
+
+        protected override float GetDistance()
+        {
+            return (Context.EndValue - Context.StartValue).Abs();
+        }
+
+        protected override float OnProcess(float normalizedTime)
+        {
+            return Mathf.Lerp(Context.StartValue, Context.EndValue, normalizedTime);
+        }
+    }
+}
