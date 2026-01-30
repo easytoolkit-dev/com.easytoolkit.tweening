1b2038d3820026247871d6880672c1fd0e90a1af
计都 <fuyouawa@gmail.com>
2026-01-30 10:53:22 +0800

    ✨更新：扩展FluxSequenceExtensions

--- Runtime/Extensions/FluxSequenceExtensions.cs
+++ Runtime/Extensions/FluxSequenceExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using EasyToolkit.Fluxion.Core;
 
 namespace EasyToolkit.Fluxion.Extensions
@@ -13,6 +14,58 @@ namespace EasyToolkit.Fluxion.Extensions
         public static IFluxSequence Join(this IFluxSequence sequence, IFlux tween)
         {
             sequence.AddFluxToLastClip(tween);
+            return sequence;
+        }
+
+        /// <summary>
+        /// Appends a callback action to the sequence.
+        /// </summary>
+        /// <param name="sequence">The sequence to append the callback to.</param>
+        /// <param name="callback">The action to execute when the callback is triggered.</param>
+        /// <returns>The sequence for fluent chaining.</returns>
+        public static IFluxSequence AppendCallback(this IFluxSequence sequence, Action callback)
+        {
+            var fluxCallback = FluxFactory.Callback(callback);
+            sequence.AddFluxAsNewClip(fluxCallback);
+            return sequence;
+        }
+
+        /// <summary>
+        /// Appends an interval delay to the sequence.
+        /// </summary>
+        /// <param name="sequence">The sequence to append the interval to.</param>
+        /// <param name="duration">The duration of the interval in seconds.</param>
+        /// <returns>The sequence for fluent chaining.</returns>
+        public static IFluxSequence AppendInterval(this IFluxSequence sequence, float duration)
+        {
+            var fluxInterval = FluxFactory.Interval(duration);
+            sequence.AddFluxAsNewClip(fluxInterval);
+            return sequence;
+        }
+
+        /// <summary>
+        /// Joins a callback action to the last clip in the sequence.
+        /// </summary>
+        /// <param name="sequence">The sequence to join the callback to.</param>
+        /// <param name="callback">The action to execute when the callback is triggered.</param>
+        /// <returns>The sequence for fluent chaining.</returns>
+        public static IFluxSequence JoinCallback(this IFluxSequence sequence, Action callback)
+        {
+            var fluxCallback = FluxFactory.Callback(callback);
+            sequence.AddFluxToLastClip(fluxCallback);
+            return sequence;
+        }
+
+        /// <summary>
+        /// Joins an interval delay to the last clip in the sequence.
+        /// </summary>
+        /// <param name="sequence">The sequence to join the interval to.</param>
+        /// <param name="duration">The duration of the interval in seconds.</param>
+        /// <returns>The sequence for fluent chaining.</returns>
+        public static IFluxSequence JoinInterval(this IFluxSequence sequence, float duration)
+        {
+            var fluxInterval = FluxFactory.Interval(duration);
+            sequence.AddFluxToLastClip(fluxInterval);
             return sequence;
         }
     }
